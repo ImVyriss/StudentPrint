0d21c29969c1e291259f8b9da030bbcd442eec75
ImVyriss <umutdaking@gmail.com>
2023-01-25 12:34:48 -0500

    version 1 student details

---
+++ src/printstudentdetails/PrintStudentDetails.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package printstudentdetails;
+
+/** Array of objects - java example : printing studentdetails
+ *  project - share to github : collaboration tool, tracking changes keep history - version control software
+ *  char [] reverse = new char[word.length()]; - reverse of word
+ * print 3 student details are name, age store in array then prints
+ * 
+ * @author RuskiNibba
+ */
+public class PrintStudentDetails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // used for user interaction printing, input
+        
+        Student[] list = new Student[3];// array of objects declaration
+        //
+        Student s1 = new Student();
+        s1.setName("peter");
+        s1.setAge(20);
+        list[0] = s1;// stored object in array
+        System.out.println(list[0].getName() + list[0].getAge());
+    }
+    
+}
+
+
+
+
+
+
+
+
+
---
+++ src/printstudentdetails/Student.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package printstudentdetails;
+
+/**
+ *define an entity Student - attributes name, sID, age et, functions / methods etc
+ * encapsulation principle = object oriented design principles -private variables then generate getters and setters
+ * @author RuskiNibba
+ */
+public class Student {
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+    private String name;
+    private int age;
+}
+
+
+
+
+
